10e847e7330f510c3a3795648f306c09342acac5
mendes-low <uskembaev07@icloud.com>
2023-12-25 15:00:36 +0600

    hw6

---
+++ JS/arrays-22.12.23/index.js
@@ -0,0 +1,69 @@
+// const list = Array()
+// const newList = new Array()
+// console.log(list);
+// console.log(newList);
+
+
+// const numbers = [0, 3.14, 9.81, 37, 98.6, 100]
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[numbers.length - 1]);
+
+// const arr = [
+//     'Asabeneh',
+//     250,
+//     true,
+//     { country: 'Finland', city: 'Helsinki' },
+//     { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
+// ] 
+// console.log(arr)
+
+
+// let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+// const companiesArray = companiesString.split('')
+// console.log(companiesArray);
+
+
+
+// const fruits = ['banana', 'orange', 'mango', 'lemon']
+// console.log(fruits[1]);
+// console.log(fruits[0]);
+// console.log(fruits[3]);
+// console.log(fruits[fruits.length - 1]);
+
+// console.log(fruits);
+// console.log(fruits[1] = fruits[0]);
+// console.log(fruits);
+
+
+// const arr = Array() 
+// console.log(arr)
+
+// const eightEmptyValues = Array(8)  
+// console.log(eightEmptyValues) 
+
+
+// const fillArr = Array(5).fill(10)
+// console.log(fillArr);
+
+// const arrFill = Array(3).fill(5)
+// console.log(arrFill);
+
+
+// const firstList = [1, 2, 3]
+// const secondList = [4, 5, 6]
+// const third = firstList.concat(secondList)
+// console.log(third);
+
+
+// const list1 = [1, 3, 3, 5, 4, 6]
+// console.log(list1.indexOf(5));
+// console.log(list1.indexOf(6));
+
+
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+
+
+
+
+
